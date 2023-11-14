1796ae3e5be25f61002db9ce11c1a0126c395b37
salmanck66 <salmanulfaris.c.k10@gmail.com>
2023-11-14 10:49:18 +0800

    suii

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ console.log("Siiiiiiiiiiiiiiiiiii")
 const suii = [1,2,4,5,6]
 const sii  = [...suii,""]
 console.log(sii);
+
+const sioo =()=>10
+
+console.log(sioo())
+
+// setInterval(()=>{
+//     console.log("zett")
+// },3000)
